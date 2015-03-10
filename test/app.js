d723b39e8037cd064fb375f2b5ea9e38f490a895
hex7c0 <carniellifrancesco@gmail.com>
2015-03-10 21:20:42 +0100

    test: improve coverage

--- test/app.js
+++ test/app.js
@@ -2,7 +2,6 @@
 /**
  * @file app test
  * @module timeout-request
- * @package timeout-request
  * @subpackage test
  * @version 0.0.1
  * @author hex7c0 <hex7c0@gmail.com>
@@ -12,41 +11,40 @@
 /*
  * initialize module
  */
-// import
-try {
-  var timeout = require('..');
-  var express = require('express');
-  var request = require('supertest');
-} catch (MODULE_NOT_FOUND) {
-  console.error(MODULE_NOT_FOUND);
-  process.exit(1);
-}
+var timeout = require('..');
+var express = require('express');
+var request = require('supertest');
 
 /*
  * test module
  */
 describe('app', function() {
 
+  var milliseconds = 800;
+  var app;
+
+  beforeEach(function(done) {
+
+    app = express();
+    done();
+  });
+
   it('should get 200 after 800 milliseconds', function(done) {
 
-    var app = express();
     app.use(timeout({
-      milliseconds: 800
-    }));
-    app.get('/', function(req, res) {
+      milliseconds: milliseconds
+    })).get('/', function(req, res) {
 
       // pass
     });
     request(app).get('/').expect(200, done);
   });
-  it('should get 200 after 800 milliseconds after header', function(done) {
+  it('should get 200 after 800 milliseconds and after header', function(done) {
 
-    var app = express();
     app.use(timeout({
       header: true,
-      milliseconds: 800
-    }));
-    app.get('/', function(req, res) {
+      milliseconds: milliseconds
+    })).get('/', function(req, res) {
 
       // pass
     });
---
+++ test/callback.js
@@ -0,0 +1,77 @@
+'use strict';
+/**
+ * @file callback test
+ * @module timeout-request
+ * @subpackage test
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+var timeout = require('..');
+var express = require('express');
+var request = require('supertest');
+var assert = require('assert');
+
+/*
+ * test module
+ */
+describe('callback', function() {
+
+  var statusCode = 408;
+  var milliseconds = 1500;
+  var data = 'ciao';
+  var app;
+
+  beforeEach(function(done) {
+
+    app = express();
+    done();
+  });
+
+  it('should get 408 after 1500 milliseconds and print "ciao"', function(done) {
+
+    app.use(timeout({
+      milliseconds: milliseconds,
+      data: data,
+      callback: function(req, res, a) {
+
+        res.status(statusCode).send(a);
+      }
+    })).get('/', function(req, res) {
+
+      // pass
+    });
+    request(app).get('/').expect(statusCode).end(function(err, res) {
+
+      assert.equal(err, null);
+      assert.equal(res.text, data);
+      done();
+    });
+  });
+  it('should get 408 after 1500 milliseconds, after header and print "ciao"',
+    function(done) {
+
+      app.use(timeout({
+        header: true,
+        milliseconds: milliseconds,
+        data: data,
+        callback: function(req, res, a) {
+
+          res.status(statusCode).send(a);
+        }
+      })).get('/', function(req, res) {
+
+        // pass
+      });
+      request(app).get('/').expect(statusCode).end(function(err, res) {
+
+        assert.equal(err, null);
+        assert.equal(res.text, data);
+        done();
+      });
+    });
+});
